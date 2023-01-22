550e9746136ec11c6a03b5e1275e5567c41bdb42
André Alves Correia <andre2013aguabranca@gmail.com>
2023-01-22 00:50:44 -0300

    aula 04
    * 'New habit' fully functional
    * 'Home' semi functional

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,5 +1,9 @@
 import { Dimensions } from "react-native";
 import {TouchableOpacity, TouchableOpacityProps} from "react-native"
+import clsx from "clsx";// usamos para estilização condicional (ex: quadradinho escuro se poucos habitos completos, quadradinho claro se muitos)
+
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
+import dayjs from "dayjs";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5
@@ -8,13 +12,32 @@ export const DAY_MARGIN_BEWTWEEN = 8;
 
 export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5)
 
-interface HabitDayProps extends TouchableOpacityProps {}
+interface HabitDayProps extends TouchableOpacityProps {
+   amountOfHabits?: number;
+   amountCompleted?: number;
+   date: Date;
+}
 
 // quadradinho que representa um dia e seus habitos
-export function HabitDay({...rest}: HabitDayProps){
-    return (
+export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...rest}: HabitDayProps){
+   
+   const accomplishmentPercentage = generateProgressPercentage(amountOfHabits, amountCompleted)
+   
+   const today = dayjs().startOf('day').toDate()
+   const isCurrentDay = dayjs(date).isSame(today)
+
+   return (
         <TouchableOpacity
-            className = "bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+            className = {clsx("rounded-lg border-2 m-1", {
+               ' bg-zinc-900  border-zinc-800': accomplishmentPercentage === 0,
+                ' bg-violet-900  border-violet-700': accomplishmentPercentage > 0 && accomplishmentPercentage < 20,
+                ' bg-violet-800  border-violet-600': accomplishmentPercentage >= 20 && accomplishmentPercentage < 40,
+                ' bg-violet-700  border-violet-500': accomplishmentPercentage >= 40 && accomplishmentPercentage < 60,
+                ' bg-violet-600  border-violet-500': accomplishmentPercentage >= 60 && accomplishmentPercentage < 80,
+                ' bg-violet-500  border-violet-400': accomplishmentPercentage >= 80,
+                'border-white border-4 bg-zinc-900': isCurrentDay
+            })}
+
             style={{width: DAY_SIZE, height: DAY_SIZE}}
             activeOpacity={0.7}
             {...rest}
--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,89 +1,119 @@
+// tela de Novo Habito
+
 import { useState } from "react";
-import { View, ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
+import { api } from "../lib/axios";
 
 const daysOfTheWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
 
 
-// tela de Novo Habito
-export function New(){
 
-    const [weekDays, setWeekDays] = useState<number[]>([])
+export function New() {
+   const [title, setTitle] = useState('')
+   const [weekDays, setWeekDays] = useState<number[]>([])
 
-    function handleToggleWeekDay(weekDayIndex: number){
-        if(weekDays.includes(weekDayIndex)) {
-            // desmarcar (remove de weekDays)
-            setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
-        } else{
-            // marcar (adiciona a weekDays)
-            setWeekDays(prevState => [...prevState, weekDayIndex ])// "spread operator"
-        }
-    }
-    
-    return (
-        <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView
-                contentContainerStyle={{paddingBottom: 100}}
+   async function handleCreateNewHabit() {
+      try {
+         if(!title.trim()){
+            Alert.alert('Novo Hábito', 'Insira nome do hábito')
+            return
+         }
+         if(weekDays.length === 0){
+            Alert.alert('Novo Hábito', 'Insira ao menos um dia')
+            return
+         }
+
+         await api.post('/habits', {title, weekDays})
+         Alert.alert('Novo Hábito', 'Hábito criado com sucesso!')
+         // reseta os estados para valores padrões.
+         setTitle('')
+         setWeekDays([])
+
+      } catch (error) {
+         console.log(error)
+         Alert.alert('Ops', 'Não foi possível criar o novo hábito')
+      }
+   }
+
+   function handleToggleWeekDay(weekDayIndex: number) {
+      if (weekDays.includes(weekDayIndex)) {
+         // desmarcar (remove de weekDays)
+         setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
+      } else {
+         // marcar (adiciona a weekDays)
+         setWeekDays(prevState => [...prevState, weekDayIndex])// "spread operator"
+      }
+   }
+
+   return (
+      <View className="flex-1 bg-background px-8 pt-16">
+         <ScrollView
+            contentContainerStyle={{ paddingBottom: 100 }}
+         >
+
+            <BackButton />
+
+            <Text
+               className="mt-6 text-white font-extrabold text-3xl" // text-3xl: tamanho grande de texto
+            >
+               Criar hábito
+            </Text>
+
+
+            <Text className="mt-6 text-white font-semibold text-base">
+               Qual seu comprometimento?
+            </Text>
+
+            <TextInput
+               className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-500"
+               placeholder="Caminhar, dormir bem, etc..."
+               placeholderTextColor={colors.zinc[400]}
+               onChangeText={setTitle}
+               value = {title}
+            />
+
+
+            <Text className="mt-4 text-white font-semibold text-base mb-3">
+               Qual a recorrência?
+            </Text>
+
+            {
+               daysOfTheWeek.map((weekDay, index) => (
+
+                  <CheckBox
+                     key={weekDay}
+                     title={weekDay}
+                     checked={weekDays.includes(index)}
+                     onPress={() => handleToggleWeekDay(index)}
+                  />
+               ))
+            }
+
+
+            <TouchableOpacity
+               // botao de confirmar o formulario
+               className="mt-6 w-full h-14 bg-green-600 flex-row items-center justify-center rounded-md"
+               activeOpacity={0.7}
+               onPress={handleCreateNewHabit}
             >
 
-                <BackButton />
+               <Feather
+                  name="check"
+                  size={20}
+                  color={colors.white}
 
-                <Text 
-                    className="mt-6 text-white font-extrabold text-3xl" // text-3xl: tamanho grande de texto
-                >
-                    Criar hábito
-                </Text>
+               />
 
+               <Text className="text-white font-semibold text-base ml-2">
+                  Confirmar
+               </Text>
+            </TouchableOpacity>
 
-                <Text className="mt-6 text-white font-semibold text-base">
-                    Qual seu comprometimento?
-                </Text>
-
-                <TextInput 
-                    className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-500"
-                    placeholder="Caminhar, dormir bem, etc..."
-                    placeholderTextColor={colors.zinc[400]}
-                />
-                
-
-                <Text className="mt-4 text-white font-semibold text-base mb-3">
-                    Qual a recorrência?
-                </Text>
-
-                {
-                    daysOfTheWeek.map((weekDay, index)=>(
-                        
-                        <CheckBox 
-                            key={weekDay} 
-                            title={weekDay}
-                            checked={weekDays.includes(index)}
-                            onPress={() => handleToggleWeekDay(index)}
-                        />
-                    ))
-                }
-
-                
-                <TouchableOpacity 
-                    className="mt-6 w-full h-14 bg-green-600 flex-row items-center justify-center rounded-md"
-                    activeOpacity={0.7}
-                >
-
-                    <Feather 
-                        name="check"
-                        size={20}
-                        color={colors.white}
-                        
-                    /> 
-
-                    <Text className="text-white font-semibold text-base ml-2">
-                        Confirmar
-                    </Text>
-                </TouchableOpacity>
-
-            </ScrollView>
-        </View>
-    )
+         </ScrollView>
+      </View>
+   )
 }
