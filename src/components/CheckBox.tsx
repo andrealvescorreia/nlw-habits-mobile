e9d14759d00fea77fe0658c3b2f3038171d4a33d
André Alves Correia <andre2013aguabranca@gmail.com>
2023-01-22 23:50:31 -0300

    Animations

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,6 +1,7 @@
 import { TouchableOpacity, View, Text, TouchableOpacityProps } from "react-native";
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import Animated, { ZoomIn, ZoomOut} from "react-native-reanimated";
 
 interface Props extends TouchableOpacityProps {
    checked?: boolean; // '?' significa que pode ser nulo
@@ -20,13 +21,17 @@ export function CheckBox({ checked = false, title, ...rest }: Props) {
          {
             checked
                ?// se checked, entao mostra ele verde e com simbolo check
-               <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+               <Animated.View 
+                  className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+                  entering={ZoomIn}
+                  exiting={ZoomOut}
+               >
                   <Feather
                      name="check"
                      size={20}
                      color={colors.white}
                   />
-               </View>
+               </Animated.View>
 
                :
                // se nao, botao cinza
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
 
 interface Props {
    progress?: number;
@@ -6,12 +8,23 @@ interface Props {
 
 //                   por padrão o progresso é 0%. max: 100%
 export function ProgressBar({ progress = 0 }: Props) {
-   return (
+   const sharedProgress = useSharedValue(progress)
+   
+   const style = useAnimatedStyle(()=>{
+      return{
+         width:  `${sharedProgress.value}%`
+      }
+   })
+   
+   useEffect(() => {
+      sharedProgress.value = withTiming(progress)
+   }, [progress])// sempre que progress atualiza, sharedProgress também irá, e com ele uma animação de transição!
 
+   return (
       <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-         <View
+         <Animated.View
             className="h-3 rounded-xl bg-violet-600"
-            style={{ width: `${progress}%` }}
+            style={style}
          />
       </View>
    )
